22906c04755dc9cd18672aba6674847105781a42
andjelamijatovic <andjelammijatovic@gmail.com>
2024-05-22 09:53:42 +0200

    initial commit

---
+++ src/app/venue/venue-details/venue-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venue-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './venue-details.component.html',
+  styleUrl: './venue-details.component.css'
+})
+export class VenueDetailsComponent {
+
+}
---
+++ src/app/venue/venue-list/venue-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venue-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './venue-list.component.html',
+  styleUrl: './venue-list.component.css'
+})
+export class VenueListComponent {
+
+}
